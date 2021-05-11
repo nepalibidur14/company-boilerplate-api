2496aa57f779338faad769c6efaab8c1aae8f8ff
nepalibidur14 <nepalibidur14@gmail.com>
2021-05-11 17:30:31 +0545

    ready for deployment

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const passport = require("passport");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 const port = process.env.port || 8000;
@@ -32,6 +34,7 @@ require("../portfolio-api/src/users/auth");
 app.use("/api/v1", require("./src/users"));
 app.use("/api/v1", require("./src/products"));
 app.use(baseUrl + "/temp", express.static("temp"));
+app.use("/", express.static("public"));
 
 // Catch any bad requests
 app.get("*", (req, res) => {
